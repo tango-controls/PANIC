3ffbe28e5c02393184733e1d27230090b41295e4
sergi_rubio <sergi_rubio@3b39871e-fe4b-0410-b5db-e82997f68959>
2013-09-03 15:53:34 +0000

    Methods for testing alarms module

---
+++ panic/test.py
@@ -0,0 +1,21 @@
+import panic
+from panic import PyAlarm
+import fandango
+
+#Create the test device
+
+#Launch it; take snapshot of memory usage
+
+
+#Load test setup
+
+#Run API methods for testing (take snapshot of memory usage)
+
+## Alarm cycle (activation/recovery/autoreset)
+
+## Test Enable/Disable alarm
+
+## 
+
+## Check memory usage increase in device and api after tests
+
